f3a031facdfa2505b3d70032c1be618b1a8acfbe
Prince <this.is.kumar.prince@gmail.com>
2021-09-26 12:36:54 +0530

    04conversion practice-1

--- 04conversion/main.go
+++ 04conversion/main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
 func main()  {
-	fmt.Println("Welcome to our Pizza App.")
-	fmt.Println("Please rate our Pizza between 1 and 5")
-
-	reader := bufio.NewReader(os.Stdin);
-
-	input , _ := reader.ReadString('\n')
-
-	fmt.Println("Thanks for rating, ",input)
-	numRating , err :=  strconv.ParseFloat(strings.TrimSpace(input),64)
-	
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Added 1 to you rating: ",numRating+1);
-		fmt.Printf("Type of numRating is, %T",numRating)
-	}
-
+	fmt.Println("04conversion ")
 }
