05aca6d7dd00a57341070de3cbdf6ba4c241932d
Prince <this.is.kumar.prince@gmail.com>
2021-09-26 12:53:11 +0530

    05mymaths practice-1

--- 05mymaths/main.go
+++ 05mymaths/main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"crypto/rand"
-	"fmt"
-	"math/big"
-)
+import "fmt"
 
-func main()  {
-	fmt.Println("Welcome To Maths In GoLang.")
-
-	// var myNumberOne int = 2
-	// var myNumberTwo float64 = 4.5
-
-	// fmt.Println("The Sum is: ",myNumberTwo+float64(myNumberOne))
-	// fmt.Println("The Sum is: ",myNumberOne+int(myNumberTwo))
-
-	// random number
-	// rand.Seed(30232)
-	// rand.Seed(time.Now().UnixNano())
-	// fmt.Println(rand.Intn(5))
-	
-	// random from crypto
-
-	myRandomNumber , _ := rand.Int(rand.Reader,big.NewInt(5))
-	fmt.Println(myRandomNumber)
+func main() {
+	fmt.Println("05mymaths")
 }
