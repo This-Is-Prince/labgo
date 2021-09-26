6a656557a3ffb4f1290f085bb153c9be0b1d5260
Prince <this.is.kumar.prince@gmail.com>
2021-09-26 12:35:37 +0530

    03userinput practice-1 done

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("03 userinput")
+	welcome := "Welcome to user input"
+	fmt.Println(welcome)
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println(input)
+	fmt.Printf("Input type is: %T \n", input)
 }
