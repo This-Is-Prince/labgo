6449660bf8270ccdf28ba7d10fc1eee802a86f9a
Prince <this.is.kumar.prince@gmail.com>
2021-09-26 13:58:30 +0530

    06mymaths practice-1 done

--- 06mytime/main.go
+++ 06mytime/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("06mytime")
+	fmt.Println("Welcome to time study of golang.")
+
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createdDate := time.Date(2000, time.September, 2, 2, 30, 15, 17, time.UTC)
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday"))
 }
