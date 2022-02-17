7ceaef90e09ae2f2d520b757a729dc29ecf935ab
Prince <this.is.kumar.prince@gmail.com>
2022-02-17 07:23:41 +0530

    mymaps completed

---
+++ hitesh_go/mygolang/10mymaps/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps in golang")
+
+	// Maps
+	languages := make(map[string]string)
+
+	languages["JS"] = "Javascript"
+	languages["RB"] = "Ruby"
+	languages["PY"] = "Python"
+
+	fmt.Println("List of all languages: ", languages)
+	fmt.Println("JS shorts for: ", languages["JS"])
+
+	delete(languages, "RB")
+	fmt.Println("List of all languages after delete: ", languages)
+
+	// Loops are interesting in golang
+
+	for key, value := range languages {
+		fmt.Printf("For key %v, value is %v\n", key, value)
+	}
+	fmt.Println("Languages length is ", len(languages))
+}
