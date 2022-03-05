7906e2e084d50a7c776cfe2bdf96573efa35e944
Prince <this.is.kumar.prince@gmail.com>
2022-03-05 16:56:57 +0530

    for completed

---
+++ go_by_example/ex_5_for/for.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("---------For---------")
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		// i = i + 1
+		// i += 1
+		i++
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("Loop")
+		break
+	}
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
