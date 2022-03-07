3166b34539114152a762603dcae738f1cd1e183e
Prince <this.is.kumar.prince@gmail.com>
2022-03-07 13:14:40 +0530

    21webreqverbs post completed

--- hitesh_go/mygolang/21webreqverbs/main.go
+++ hitesh_go/mygolang/21webreqverbs/main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	fmt.Println("--------Web Request Verbs--------")
 
-	PerformGetRequest()
+	// PerformGetRequest()
+	PerformPostJsonRequest()
 }
 
 func PerformGetRequest() {
@@ -36,4 +37,38 @@ func PerformGetRequest() {
 
 	// fmt.Println(content)
 	// fmt.Println(string(content))
+}
+
+func PerformPostJsonRequest() {
+	const myUrl = "http://localhost:8000/post"
+
+	// fake json payload
+	requestBody := strings.NewReader(`
+		{
+			"coursename":"Let's go with golang",
+			"price": 0,
+			"platform":"learnCodeOnline.in"
+		}
+	`)
+	response, err := http.Post(myUrl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// 1.first
+	// var responseString strings.Builder
+	// noOfBytes, err := responseString.Write(content)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("No of bytes returned from post request:", noOfBytes)
+	// fmt.Println("Response return from post request:", responseString.String())
+
+	// 2.second
+	fmt.Println(string(content))
 }
