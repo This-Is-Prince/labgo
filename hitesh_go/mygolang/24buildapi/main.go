c314e09fa6ac62d5e6b4cac7633f9eaf215a5d46
Prince <this.is.kumar.prince@gmail.com>
2022-03-08 20:54:56 +0530

    get one course method completed

--- hitesh_go/mygolang/24buildapi/main.go
+++ hitesh_go/mygolang/24buildapi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Model for course - file
@@ -39,6 +41,7 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to API by LearnCodeOnline</h1>"))
 }
 
+// get all courses
 func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
@@ -46,4 +49,23 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// get one course
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	// loop through courses, find matching id and return the response
+	for _, course := range courses {
+		if course.Id == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course found with given id.")
+	return
 }
