43ad9c3dc0f58e31ff30921a292034d39105219f
Prince <this.is.kumar.prince@gmail.com>
2022-03-08 20:49:06 +0530

    sending a API json response method completed

--- hitesh_go/mygolang/24buildapi/main.go
+++ hitesh_go/mygolang/24buildapi/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // Model for course - file
 type Course struct {
 	Id     string  `json:"id"`
@@ -24,4 +30,20 @@ func (c *Course) IsEmpty() bool {
 
 func main() {
 
+}
+
+// controllers - file
+
+// serve home route
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to API by LearnCodeOnline</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(courses)
+	if err != nil {
+		panic(err)
+	}
 }
