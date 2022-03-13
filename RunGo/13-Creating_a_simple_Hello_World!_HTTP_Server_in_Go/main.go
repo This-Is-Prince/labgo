60e37d165fcdb2ce1ea0969cc1eb01199a3cd5f3
Prince <this.is.kumar.prince@gmail.com>
2022-03-13 12:58:34 +0530

    simple hello world api created

---
+++ RunGo/13-Creating_a_simple_Hello_World!_HTTP_Server_in_Go/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type HttpHandler struct {
+}
+
+// implement `ServeHTTP` method on `HttpHandler` struct
+func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	/* 	// create response binary data
+	   	data := []byte(`Hello World!`) // slice of bytes
+
+	   	// write `data` to response
+	   	res.Write(data) */
+
+	// write `Hello` using `io.WriteString` function
+	io.WriteString(res, "Hello")
+
+	// write `World` using `fmt.Fprint` function
+	fmt.Fprint(res, " World! ")
+
+	// write `❤️` using simple `Write` call
+	res.Write([]byte("❤️"))
+}
+
+func main() {
+	// create a new handler
+	handler := HttpHandler{}
+
+	// listen and serve
+	http.ListenAndServe(":9000", handler)
+}
