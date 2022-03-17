dae0f4d4e161c91cfa77b6dc1a6f0edfdc25d67e
Prince <this.is.kumar.prince@gmail.com>
2022-03-17 09:27:09 +0530

    external executable execute in main.go

--- RunGo/OS_&_IO/Executing_shell_commands_script_files_and_executables_in_Go/main.go
+++ RunGo/OS_&_IO/Executing_shell_commands_script_files_and_executables_in_Go/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -52,7 +52,19 @@ func main() {
 		Stdout: os.Stdout,
 		Stderr: os.Stdout,
 	}
-
+	fmt.Println()
+	fmt.Println("Args:-", cmdGoVer.Args)
+	fmt.Println("Dir:-", cmdGoVer.Dir)
+	fmt.Println("Env:-", cmdGoVer.Env)
+	fmt.Println("ExtraFiles:-", cmdGoVer.ExtraFiles)
+	fmt.Println("Path:-", cmdGoVer.Path)
+	fmt.Println("Process:-", cmdGoVer.Process)
+	fmt.Println("ProcessState:-", cmdGoVer.ProcessState)
+	fmt.Println("Stderr:-", cmdGoVer.Stderr)
+	fmt.Println("Stdin:-", cmdGoVer.Stdin)
+	fmt.Println("Stdout:-", cmdGoVer.Stdout)
+	fmt.Println(cmdGoVer.SysProcAttr)
+	fmt.Println()
 	// see command represented by `cmdGoVer`
 	fmt.Println(cmdGoVer.String())
 
@@ -60,12 +72,12 @@ func main() {
 	if err := cmdGoVer.Run(); err != nil {
 		log.Fatal(err)
 	}
-}
-*/
+} */
 
 /* ============== 4) Start and Wait methods ============== */
-
+/*
 func main() {
+	fmt.Println(exec.LookPath("./sleep.sh"))
 	// construct `sleep.sh` command
 	cmd := &exec.Cmd{
 		Path:   "./sleep.sh",
@@ -86,4 +98,43 @@ func main() {
 	// wait `cmd` until it finishes
 	err = cmd.Wait()
 	fmt.Println(err)
+}
+*/
+
+/* ============== executable/main.exe  ============== */
+
+/*
+func main() {
+	fmt.Println("`HELLO` Env:-", os.Getenv("HELLO"))
+	for index, arg := range os.Args {
+		fmt.Printf("Arg %v). %v\n", index, arg)
+	}
+}
+*/
+
+func main() {
+	mainExectablePath, err := exec.LookPath("./executable/main.exe")
+	if err != nil {
+		log.Fatal("mainExecutablePath:-", err)
+	}
+	stdOut, err := os.OpenFile("./std-streams/out.txt", os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		log.Fatal("stdOut:-", err)
+	}
+	stdErr, err := os.OpenFile("./std-streams/err.txt", os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		log.Fatal("stdErr:-", err)
+	}
+
+	cmd := &exec.Cmd{
+		Path:   mainExectablePath,
+		Args:   []string{mainExectablePath, "First Arg", "Second Arg"},
+		Env:    []string{"HELLO=PRINCE"},
+		Stdout: stdOut,
+		Stderr: stdErr,
+	}
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
