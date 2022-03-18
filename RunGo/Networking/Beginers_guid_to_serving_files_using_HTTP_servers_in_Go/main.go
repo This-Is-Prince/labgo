3d962457f73f30608dea9011563ebafb9b70b133
Prince <this.is.kumar.prince@gmail.com>
2022-03-18 21:25:19 +0530

    serving files on a route completed

--- RunGo/Networking/Beginers_guid_to_serving_files_using_HTTP_servers_in_Go/main.go
+++ RunGo/Networking/Beginers_guid_to_serving_files_using_HTTP_servers_in_Go/main.go
@@ -4,27 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
-/* ========== http.FileServer() function and http.Dir() type alias ========== */
-/*
 func main() {
-	// create file server handler
-	fs := http.FileServer(http.Dir("./tmp"))
+	fmt.Println("Beginners Guide to serving files using http servers")
 
-	// start HTTP Server with `fs` as the default handler
+	// serving_A_Directory()
+	serving_Files_On_A_Route()
+}
+
+func serving_A_Directory() {
+	// create file server handler
+	currDir, err := os.Getwd()
+	fmt.Println(currDir, err)
+	dirPath := filepath.Join(currDir, "tmp")
+	fmt.Println(dirPath)
+	fs := http.FileServer(http.Dir(dirPath))
+
+	// start HTTP server with `fs` as the default handler
 	log.Fatal(http.ListenAndServe(":9000", fs))
 }
-*/
 
-/* ========== Serving files on a route ========== */
-/*
-func main() {
+func serving_Files_On_A_Route() {
 	// create file server handler
-	fs := http.FileServer(http.Dir("./tmp"))
+	currDir, _ := os.Getwd()
+	fs := http.FileServer(http.Dir(filepath.Join(currDir, "tmp")))
 
-	// handle `/` route
+	// handler `/` route
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-Type", "text/html")
 		fmt.Fprint(res, "<h1>Golang!</h1>")
@@ -32,47 +40,6 @@ func main() {
 
 	// handle `/static` route
 	http.Handle("/static/", http.StripPrefix("/static", fs))
-
-	// start HTTP server with `http.DefaultServeMux` handler
-	log.Fatal(http.ListenAndServe(":9000", nil))
-}
-*/
-
-/* ========== ServeFile function ========== */
-/*
-// temporary directory location
-const cwd = "D:/Languages/Go/learning-go/RunGo/14-Beginers_guid_to_serving_files_using_HTTP_servers_in_Go"
-
-var tmpDir = filepath.FromSlash(cwd + "/tmp/")
-
-func main() {
-	// return a `.pdf` file for `/pdf` route
-	http.HandleFunc("/pdf", func(res http.ResponseWriter, req *http.Request) {
-		http.ServeFile(res, req, filepath.Join(tmpDir, "/files/test.pdf"))
-	})
-
-	// start HTTP server with `http.DefaultServeMux` handler
-	log.Fatal(http.ListenAndServe(":9000", nil))
-}
-*/
-
-/* ========== Handling index.html ========== */
-
-// temporary directory location
-const cwd = "D:/Languages/Go/learning-go/RunGo/14-Beginers_guid_to_serving_files_using_HTTP_servers_in_Go"
-
-var tmpDir = filepath.FromSlash(cwd + "/tmp/")
-
-func main() {
-	// default route
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello Golang!")
-	})
-
-	// return a `.html` file for `/index.html` route
-	http.HandleFunc("/index.html", func(res http.ResponseWriter, req *http.Request) {
-		http.ServeFile(res, req, filepath.Join(cwd, "/index.html"))
-	})
 
 	// start HTTP server with `http.DefaultServeMux` handler
 	log.Fatal(http.ListenAndServe(":9000", nil))
