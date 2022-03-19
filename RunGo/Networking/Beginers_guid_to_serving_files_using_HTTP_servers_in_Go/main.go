474a97623aaa61850f43973d35931e3ac7274dfb
Prince <this.is.kumar.prince@gmail.com>
2022-03-19 06:56:07 +0530

    guide to serving files using http server is completed

--- RunGo/Networking/Beginers_guid_to_serving_files_using_HTTP_servers_in_Go/main.go
+++ RunGo/Networking/Beginers_guid_to_serving_files_using_HTTP_servers_in_Go/main.go
@@ -12,15 +12,19 @@ func main() {
 	fmt.Println("Beginners Guide to serving files using http servers")
 
 	// serving_A_Directory()
-	serving_Files_On_A_Route()
+	// serving_Files_On_A_Route()
+	// serveFile_Function()
+	handling_Index_HTML()
 }
 
 func serving_A_Directory() {
 	// create file server handler
 	currDir, err := os.Getwd()
-	fmt.Println(currDir, err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	dirPath := filepath.Join(currDir, "tmp")
-	fmt.Println(dirPath)
+	fmt.Println("DirPath:", dirPath)
 	fs := http.FileServer(http.Dir(dirPath))
 
 	// start HTTP server with `fs` as the default handler
@@ -29,7 +33,10 @@ func serving_A_Directory() {
 
 func serving_Files_On_A_Route() {
 	// create file server handler
-	currDir, _ := os.Getwd()
+	currDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
 	fs := http.FileServer(http.Dir(filepath.Join(currDir, "tmp")))
 
 	// handler `/` route
@@ -40,6 +47,46 @@ func serving_Files_On_A_Route() {
 
 	// handle `/static` route
 	http.Handle("/static/", http.StripPrefix("/static", fs))
+
+	// start HTTP server with `http.DefaultServeMux` handler
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
+
+func serveFile_Function() {
+	currDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpDir := filepath.FromSlash(currDir)
+	// return a `.pdf` file for `/pdf` route
+	http.HandleFunc("/pdf", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Println(req.URL.Path)
+		http.ServeFile(res, req, filepath.Join(tmpDir+"/tmp/files/test.pdf"))
+	})
+
+	// start HTTP server with `http.DefaultServeMux` handler
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
+
+func handling_Index_HTML() {
+	// working directory
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// removing `slash` with platform dependant `\`, `/`
+	currDir := filepath.FromSlash(wd)
+
+	// default route
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "Hello Golang!")
+	})
+
+	// return a `.html` file for `/index.html` route
+	http.HandleFunc("/index.html", func(res http.ResponseWriter, req *http.Request) {
+		http.ServeFile(res, req, filepath.Join(currDir, "/index.html"))
+	})
 
 	// start HTTP server with `http.DefaultServeMux` handler
 	log.Fatal(http.ListenAndServe(":9000", nil))
