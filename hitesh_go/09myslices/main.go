f5bd95283655f78af6c1a889444d19b7f3d2a8d8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-04-24 06:52:58 +0530

    slices completed

---
+++ hitesh_go/09myslices/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Welcome to video on slices")
+
+	var fruitList = []string{"Mango", "Tomato", "Peach"}
+	fmt.Println("FruitList is:", fruitList)
+	fmt.Println("FruitList length is:", len(fruitList))
+	fmt.Printf("Type of fruitList is %T\n", fruitList)
+
+	fruitList = append(fruitList, "Apple", "Banana")
+	fmt.Println("FruitList is:", fruitList)
+	fmt.Println("FruitList length is:", len(fruitList))
+	fmt.Printf("Type of fruitList is %T\n", fruitList)
+
+	// fruitList = append(fruitList[1:3])
+	fruitList = fruitList[1:]
+	fmt.Println("FruitList is:", fruitList)
+	fmt.Println("FruitList length is:", len(fruitList))
+	fmt.Printf("Type of fruitList is %T\n", fruitList)
+
+	highScores := make([]int, 4)
+	highScores[0] = 234
+	highScores[1] = 945
+	highScores[2] = 465
+	highScores[3] = 867
+	fmt.Println(highScores)
+	highScores = append(highScores, 555, 666, 321)
+	fmt.Println(highScores)
+	fmt.Println(sort.IntsAreSorted(highScores))
+	sort.Ints(highScores)
+	fmt.Println(highScores)
+	fmt.Println(sort.IntsAreSorted(highScores))
+}
