0697ef8ae5ee6962dea19ea5ac97dd924d39c438
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-04-29 07:22:20 +0530

    defer chapter completed

---
+++ hitesh_go/17defer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("World")
+	defer fmt.Println("One")
+	defer fmt.Println("Two")
+	fmt.Println("Hello")
+	myDefer()
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		if i == 0 {
+			defer fmt.Println(i)
+		} else {
+			defer fmt.Print(i, ", ")
+		}
+	}
+}
