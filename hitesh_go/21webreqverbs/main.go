1fad8431bf9f475effa92148dd7bf4745c9ad334
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-03 15:53:18 +0530

    postform chapter completed

--- hitesh_go/21webreqverbs/main.go
+++ hitesh_go/21webreqverbs/main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -13,10 +14,36 @@ func main() {
 	fmt.Println("Welcome to web verb video - LCO")
 
 	// PerformGetRequest()
-	PerformJsonRequest()
+	// PerformPostJsonRequest()
+	PerformPostFormRequest()
 }
 
-func PerformJsonRequest() {
+func PerformPostFormRequest() {
+	const myUrl = "http://localhost:8000/post"
+
+	// formdata
+	data := url.Values{}
+	data.Add("firstname", "prince")
+	data.Add("lastname", "kumar")
+	data.Add("email", "prince@go.dev")
+
+	response, err := http.PostForm(myUrl, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+	contentInByte, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var contentInString strings.Builder
+	contentInString.Write(contentInByte)
+	fmt.Println(contentInString.String())
+}
+
+func PerformPostJsonRequest() {
 	const myUrl = "http://localhost:8000/post"
 
 	// fake json payload
