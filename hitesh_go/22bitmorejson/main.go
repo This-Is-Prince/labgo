3d0898148ab714304174e51eb89fef4a0aeb8581
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-04 10:27:27 +0530

    json unmarshal completed

--- hitesh_go/22bitmorejson/main.go
+++ hitesh_go/22bitmorejson/main.go
@@ -17,7 +17,42 @@ type course struct {
 func main() {
 	fmt.Println("Welcome to JSON video")
 
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
+}
+
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+        {
+                "coursename": "ReactJS Bootcamp",
+                "price": 299,
+                "website": "LearnCodeOnline.in",
+                "tags": ["web-dev","js"]
+        }
+	`)
+
+	var locCourse course
+	checkValid := json.Valid(jsonDataFromWeb)
+	if checkValid {
+		fmt.Println("JSON was valid")
+		err := json.Unmarshal(jsonDataFromWeb, &locCourse)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%#v\n", locCourse)
+	}
+
+	// some cases where you just want to add data to key value
+	var myOnlineData map[string]interface{}
+	err := json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("Key is %v and value is %v and Type is %T\n", k, v, v)
+	}
 }
 
 func EncodeJson() {
