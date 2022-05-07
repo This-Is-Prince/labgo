f0c5ee1ef480862a540a26ad92c118023d108026
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-08 03:43:47 +0530

    add a course controller completed

--- hitesh_go/24buildapi/main.go
+++ hitesh_go/24buildapi/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -26,7 +29,8 @@ var courses []Course
 
 // middleware, helper - file
 func (c *Course) IsEmpty() bool {
-	return c.Id == "" && c.Name == ""
+	// return c.Id == "" && c.Name == ""
+	return c.Name == ""
 }
 
 func main() {
@@ -63,4 +67,34 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode("No Course found with given id")
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// what if: body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+		return
+	}
+
+	// What about - {}
+
+	var course Course
+	err := json.NewDecoder(r.Body).Decode(&course)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return
+	}
+
+	// generate unique id, string
+	rand.Seed(time.Now().UnixNano())
+	course.Id = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
 }
