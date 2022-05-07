277b3b981448c620cb91141e00204a20750f3200
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-07 23:40:05 +0530

    2 routes completed

--- hitesh_go/24buildapi/main.go
+++ hitesh_go/24buildapi/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 // Model for course - file
 type Course struct {
@@ -25,4 +29,20 @@ func (c *Course) IsEmpty() bool {
 
 func main() {
 	fmt.Println("Building API")
+
+}
+
+// controllers - file
+
+// serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`<h1>Welcome to API by LearnCodeOnline</h1>`))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+
 }
