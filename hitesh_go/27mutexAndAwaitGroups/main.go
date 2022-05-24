16fbd329fea948d09933e0d8ddecaa182ff9c7d6
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-24 11:30:09 +0530

    read write mutex completed

---
+++ hitesh_go/27mutexAndAwaitGroups/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fmt.Println("Race condition")
+
+	wg := &sync.WaitGroup{}
+	mut := &sync.RWMutex{}
+
+	mut.RLock()
+	var score = []int{0}
+	mut.RUnlock()
+
+	wg.Add(4)
+	go func(wg *sync.WaitGroup, m *sync.RWMutex) {
+		fmt.Println("One Routine")
+
+		m.Lock()
+		score = append(score, 1)
+		m.Unlock()
+
+		wg.Done()
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, m *sync.RWMutex) {
+		fmt.Println("Two Routine")
+
+		m.Lock()
+		score = append(score, 2)
+		m.Unlock()
+
+		wg.Done()
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, m *sync.RWMutex) {
+		fmt.Println("Three Routine")
+
+		m.Lock()
+		score = append(score, 3)
+		m.Unlock()
+
+		wg.Done()
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, m *sync.RWMutex) {
+		fmt.Println("Four Routine")
+
+		m.RLock()
+		fmt.Println(score)
+		m.RUnlock()
+
+		wg.Done()
+	}(wg, mut)
+
+	wg.Wait()
+
+	mut.RLock()
+	fmt.Println(score)
+	mut.RUnlock()
+}
