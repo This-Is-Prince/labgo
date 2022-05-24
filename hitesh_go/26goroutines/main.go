362ee47cdc4debfe86a82e4aa7cf1120e45fbce8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-24 10:39:01 +0530

    go routines started

---
+++ hitesh_go/26goroutines/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Go Routines")
+
+	go greeter("Hello")
+	greeter("World")
+}
+
+func greeter(s string) {
+	for i := 0; i < 6; i++ {
+		time.Sleep(3 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
