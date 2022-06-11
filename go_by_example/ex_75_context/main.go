b2be3686ac2af84c5c92cccad1fe3dea858a36fb
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-06-11 13:21:07 +0530

    context completed

---
+++ go_by_example/ex_75_context/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
+	defer req.Body.Close()
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	fmt.Println("Context")
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":4000", nil)
+}
