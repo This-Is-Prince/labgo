225b52f5b443fced2c8a17fbc9a7e54cb5fc4d41
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-07-12 22:52:49 +0530

    feat: example 2 values completed

---
+++ go_by_example_revised/ex_02_values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	languageName := "go" + "lang1"
+	fmt.Println("go" + "lang")
+	fmt.Println(languageName)
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
