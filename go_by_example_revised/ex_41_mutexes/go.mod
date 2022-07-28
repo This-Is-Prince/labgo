537c57502b31405d76156c0153ec9892579972ee
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-07-28 22:49:09 +0530

    feat: example 42 mutexes completed

---
+++ go_by_example_revised/ex_41_mutexes/go.mod
@@ -0,0 +1,3 @@
+module github.com/This-Is-Prince/mutexes
+
+go 1.18
---
+++ go_by_example_revised/ex_41_mutexes/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Container struct {
+	mu       sync.Mutex
+	counters map[string]int
+}
+
+func (c *Container) inc(name string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.counters[name]++
+}
+
+func main() {
+	fmt.Println("Mutexes")
+
+	c := Container{
+		counters: map[string]int{
+			"a": 0,
+			"b": 0,
+		},
+	}
+
+	var wg sync.WaitGroup
+
+	doIncrement := func(name string, n int) {
+		defer wg.Done()
+		for i := 0; i < n; i++ {
+			c.inc(name)
+		}
+	}
+
+	wg.Add(3)
+	go doIncrement("a", 1000)
+	go doIncrement("a", 1000)
+	go doIncrement("b", 1000)
+
+	wg.Wait()
+	fmt.Println(c.counters)
+}
