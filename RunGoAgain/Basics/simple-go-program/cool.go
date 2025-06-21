317aafb88c063e0d2b038cb809cebffe08b4c56c
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-06-22 05:27:47 +0530

    feat: simple go program rungo

---
+++ RunGoAgain/Basics/simple-go-program/cool.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func PrintCool() {
+	fmt.Println("Cool!")
+}
+
+/*
+So this is simple go Program that prints cool! to STDOUT.
+To build this program, use the command: go build *.go or go build main.go cool.go
+*/
---
+++ RunGoAgain/Basics/simple-go-program/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hey")
+	PrintCool()
+}
+
+// This is a simple go program that prints hey to the STDOUT.
+// To run this program, use the command: go run main.go
