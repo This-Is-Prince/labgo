e18480f6a57fea3349ac8e03a91e17289a35834b
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-07-06 23:09:00 +0530

    feat: serving files

---
+++ RunGoAgain/Networking/beginners-guide-to-serving-files-using-http-servers/go.mod
@@ -0,0 +1,3 @@
+module github.com/RunGoAgain/Networking/beginners-guide-to-serving-files-using-http-servers
+
+go 1.24.4
---
+++ RunGoAgain/Networking/beginners-guide-to-serving-files-using-http-servers/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./tmp"))
+
+	// log.Fatal(http.ListenAndServe(":9000", fs))
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(res, `<h1>Golang!</h1> <a href="/static/hi.html">Hi</a>`)
+	})
+
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
