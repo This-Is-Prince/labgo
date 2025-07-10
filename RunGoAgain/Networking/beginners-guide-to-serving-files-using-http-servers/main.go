4893c79a061876ec2d3d55be9eb81664909367f8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-07-10 14:55:46 +0530

    feat: serving files using http servers

--- RunGoAgain/Networking/beginners-guide-to-serving-files-using-http-servers/main.go
+++ RunGoAgain/Networking/beginners-guide-to-serving-files-using-http-servers/main.go
@@ -16,6 +16,10 @@ func main() {
 		fmt.Fprint(res, `<h1>Golang!</h1> <a href="/static/hi.html">Hi</a>`)
 	})
 
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./tmp/hi.html")
+	})
+
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 
 	log.Fatal(http.ListenAndServe(":9000", nil))
