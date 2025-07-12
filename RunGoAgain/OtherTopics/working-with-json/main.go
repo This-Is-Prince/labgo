b8cc35d157e3f97138d569f7675dbfbf49a6e644
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-07-12 10:48:43 +0530

    feat: json learning

--- RunGoAgain/OtherTopics/working-with-json/main.go
+++ RunGoAgain/OtherTopics/working-with-json/main.go
@@ -57,6 +57,39 @@ type Student3 struct {
 	Profile   Profile1 `json:""`
 }
 
+type Student4 struct {
+	FirstName, lastName string
+	Email               string
+	Age                 int
+	HeightInMeters      float64
+}
+
+type Profile2 struct {
+	Username  string
+	Followers int
+}
+
+type Student5 struct {
+	FirstName, lastName string
+	HeightInMeters      float64
+	IsMale              bool
+	Languages           [2]string
+	Subjects            []string
+	Grades              map[string]string
+	// Profile2            Profile2
+	Profile2
+}
+
+type Student6 struct {
+	FirstName, lastName string
+	HeightInMeters      float64
+	IsMale              bool
+	Languages           [2]string
+	Subjects            []string `json:",omitempty"`
+	Grades              map[string]string
+	Profile             *Profile2
+}
+
 func main() {
 
 	john := Student{
@@ -112,5 +145,87 @@ func main() {
 
 	john3JSON, _ := json.MarshalIndent(john3, "", "  ")
 	fmt.Println(string(john3JSON))
+	fmt.Println()
+
+	data := []byte(`
+	{
+		"FirstName": "John",
+		"lastName": "Doe",
+		"Age": 20,
+		"HeightInMeters": 1.75,
+		"Username": "johndoe91"
+	}
+	`)
+
+	var john4 Student4
+	err := json.Unmarshal(data, &john4)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return
+	}
+	fmt.Printf("%#v\n", john4)
+	fmt.Println()
+
+	data1 := []byte(`
+	{
+		"FirstName": "John",
+		"HeightInMeters": 1.75,
+		"IsMale": null,
+		"Languages": ["English", "Spanish", "German"],
+		"Subjects": ["Math", "Science"],
+		"Grades": { "Math": "A", "Art": "B" },
+		"Profile2": {
+			"Username": "johndoe91",
+			"Followers": 1975
+		}
+	}
+	`)
+	// data1 := []byte(`
+	// {
+	// 	"FirstName": "John",
+	// 	"HeightInMeters": 1.75,
+	// 	"IsMale": null,
+	// 	"Languages": ["English", "Spanish", "German"],
+	// 	"Subjects": ["Math", "Science"],
+	// 	"Grades": { "Math": "A", "Art": "B" },
+	// 	"Username": "johndoe91",
+	// 	"Followers": 1975
+	// }
+	// `)
+
+	var john5 Student5 = Student5{
+		IsMale:    true,
+		Languages: [2]string{"Russian"},
+		Subjects:  []string{"Art"},
+		Grades:    map[string]string{"Science": "A+"},
+	}
+
+	fmt.Printf("Error: %v\n", json.Unmarshal(data1, &john5))
+	fmt.Printf("%#v\n", john5)
+	fmt.Println()
+
+	data2 := []byte(`
+	{
+		"FirstName": "John",
+		"HeightInMeters": 1.75,
+		"IsMale": null,
+		"Languages": ["English", "Spanish", "German"],
+		"Subjects": null,
+		"Grades": null,
+		"Profile": { "Followers": 1975 }
+	}
+	`)
+
+	var john6 Student6 = Student6{
+		IsMale:    true,
+		Languages: [2]string{"Russian"},
+		Subjects:  []string{"Art"},
+		Grades:    map[string]string{"Math": "A"},
+		Profile:   &Profile2{Username: "johndoe91"},
+	}
+
+	fmt.Printf("Error: %v\n", json.Unmarshal(data2, &john6))
+	fmt.Printf("%#v\n", john6)
+	fmt.Printf("%#v\n", john6.Profile)
 	fmt.Println()
 }
